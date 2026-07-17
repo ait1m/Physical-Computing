4e1196e196810567a67bec7acafddf070ba53d3f
ait1m <68070042@kmitl.ac.th>
2026-07-17 15:50:29 +0700

    -

--- LAB002/q04_Time_Format.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int hour, min;
-    scanf("%d\n%d",&hour,&min);
-    printf("%02d:%02d",hour,min);
-
-    return 0;
-};
--- LAB002/q05_Greeting.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char c[200];
-    scanf("%[^\n]",c);
-    printf("Hello, %s!",c);
-
-    return 0;
-};
